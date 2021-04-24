785fc1573dd48ba84fa94dc8809998ebd21d0161
Oksana Mironova <kq3hip13z@gmail.com>
2021-04-24 12:42:35 +0200

    Order button function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './style.css';
 
+/*Na tlačíko .order-btn pověste posluchač, který při kliknutí přidá na prvek .drink__cup třídu .drink__cup--selected. Zároveň změní nápis na tlačítku na Zrušit.*/
+
 console.log('funguju!');
+
+const btn = document.querySelector('.order-btn');
+let ordered = false;
+btn.addEventListener('click', (event) => {
+  const order = document.querySelector('.drink__cup');
+
+  if (ordered === false) {
+    order.classList.add('drink__cup--selected');
+    btn.textContent = 'Zrušit';
+    ordered = true;
+  } else {
+    order.classList.remove;
+    btn.textContent = 'Objednat';
+    ordered = false;
+  }
+});
